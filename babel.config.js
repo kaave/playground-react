422be9c33fde986206f71ac826f09934a5df7847
kaave <junkjunctions@gmail.com>
2020-03-14 14:08:15 +0900

    refactor: remove verbose plugins

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  presets: [['@babel/preset-env', process.env.NODE_ENV !== 'test' ? { modules: false } : {}], '@babel/preset-typescript', '@babel/preset-react'],
+  presets: [
+    ['@babel/preset-env', process.env.NODE_ENV !== 'test' ? { modules: false } : {}],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
   plugins: [
     /**
      * for TypeScript
      */
     '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-class-properties',
   ],
 };
